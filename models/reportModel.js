c312b8e05fa411b59fa4d68bdbcbbdef07b5255b
JD <jaysondorilag1@gmail.com>
2024-11-21 22:46:11 +0800

    refactor: Enhance report schema and validation for missing person details, add file upload handling

--- models/reportModel.js
+++ models/reportModel.js
@@ -8,7 +8,8 @@ const reportSchema = new mongoose.Schema(
       required: true,
     },
     missingPerson: {
-      name: { type: String, required: true },
+      firstname: { type: String, required: true },
+      lastname: { type: String, required: true },
       dateOfBirth: { type: Date, required: true },
       age: { type: Number, required: true },
       assignedSexAtBirth: { type: String, enum: ["Male", "Female"], required: true },
@@ -30,22 +31,27 @@ const reportSchema = new mongoose.Schema(
       eyeColor: { type: String },
       wearsContactLenses: { type: Boolean, default: false },
       bloodType: { type: String },
+      reward: { type: Number },
       images: [
         {
-          public_id: { type: String, default: "" }, 
-          url: { type: String, default: "" }, 
+          public_id: { type: String, default: "", required: true }, 
+          url: { type: String, default: "", required: true }, 
         },
       ],
-      video: [
-        {
-          public_id: { type: String, default: "" },
-          url: { type: String, default: "" },
-        },
-      ],
+      video: {
+        public_id: { type: String, default: "" },
+        url: { type: String, default: "" },
+      },
       medication: { type: String },
       birthDefects: { type: String },
-      contactNumber: { type: String },
+      contactNumber: { type: String, required: true },
       socialMediaAccount: { type: String },
+    },
+    status: {
+      type: String,
+      enum: ["Pending", "Confirmed", "Solved"],
+      default: "Pending",
+      required: true,
     },
     createdAt: { type: Date, default: Date.now },
   },
